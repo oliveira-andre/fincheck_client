71ac5ec91da1d0833265fccd3b95dcce4d8b192b
Andre Oliveira <andreoliveirar2d2@gmail.com>
2025-12-25 20:14:19 -0300

    feat: add fab on bottom of the screen and add options with icons

---
+++ src/view/pages/Dashboard/components/Fab/index.tsx
@@ -0,0 +1,35 @@
+import { PlusIcon } from "@radix-ui/react-icons";
+import { DropdownMenu } from "../../../../components/DropdownMenu";
+import { CategoryIcon } from "../../../../components/icons/categories/CategoryIcon";
+import { BankAccountIcon } from "../../../../components/icons/BankAccountIcon";
+
+export function Fab() {
+  return (
+    <div className="fixed right-4 bottom-4">
+      <DropdownMenu.Root>
+        <DropdownMenu.Trigger>
+          <button className="text-white bg-teal-900 rounded-full w-12 h-12 flex items-center justify-center">
+            <PlusIcon className="w-6 h-6" />
+          </button>
+        </DropdownMenu.Trigger>
+
+        <DropdownMenu.Content>
+          <DropdownMenu.Item className="gap-2">
+            <CategoryIcon type="expense" />
+            <span>Nova Despesa</span>
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item className="gap-2">
+            <CategoryIcon type="income" />
+            <span>Nova Receita</span>
+          </DropdownMenu.Item>
+
+          <DropdownMenu.Item className="gap-2">
+            <BankAccountIcon />
+            <span>Nova Conta</span>
+          </DropdownMenu.Item>
+        </DropdownMenu.Content>
+      </DropdownMenu.Root>
+    </div>
+  )
+}
--- src/view/pages/Dashboard/index.tsx
+++ src/view/pages/Dashboard/index.tsx
@@ -3,6 +3,7 @@ import { UserMenu } from "../../components/UserMenu";
 import { Accounts } from "./components/Accounts";
 import { Transactions } from "./components/Transactions";
 import { DashboardProvider } from "./components/DashboardContext";
+import { Fab } from "./components/Fab";
 
 export function Dashboard() {
   return (
@@ -22,6 +23,8 @@ export function Dashboard() {
             <Transactions />
           </div>
         </main>
+
+        <Fab />
       </div>
     </DashboardProvider>
   )
