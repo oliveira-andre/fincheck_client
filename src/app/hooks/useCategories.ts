ddc832c9ef7a376905a5a3314c55ab340dfe5591
Andre Oliveira <andreoliveirar2d2@gmail.com>
2026-01-15 23:34:23 -0300

    fix: matching overload  call

--- src/app/hooks/useCategories.ts
+++ src/app/hooks/useCategories.ts
@@ -5,7 +5,7 @@ import { categoriesService } from "../services/categoriesService";
 export function useCategories() {
   const { data, isFetching } = useQuery({
     queryKey: ['categories'],
-    queryFn: categoriesService.getAll,
+    queryFn: () => categoriesService.getAll({}),
   });
 
   return {
